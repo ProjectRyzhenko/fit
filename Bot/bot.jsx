12975a322feeee73c909e591066850f168411c05
School <111399819+ProjectRyzhenko@users.noreply.github.com>
2022-08-25 10:38:18 +0300

    Update bot.jsx

--- Bot/bot.jsx
+++ Bot/bot.jsx
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "5684345471:AAGzMO7XEor25knqsq4i7xuJR06xy-um2bw";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "fit-projectryzhenko.vercel.app";
+const web_link = "https://fit-red.vercel.app";
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
