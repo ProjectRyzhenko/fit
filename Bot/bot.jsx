a012890037bad88ee8c788db20474de73b95bf8c
School <111399819+ProjectRyzhenko@users.noreply.github.com>
2022-08-25 10:30:33 +0300

    Update bot.jsx

--- Bot/bot.jsx
+++ Bot/bot.jsx
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5690577020:AAEzc-aCy0e_DL8pMv4aqg0bRPgZFz_tCXU";
+const TOKEN = "5684345471:AAGzMO7XEor25knqsq4i7xuJR06xy-um2bw";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "fit-projectryzhenko.vercel.app";
